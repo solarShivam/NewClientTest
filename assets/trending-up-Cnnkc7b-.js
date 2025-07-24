0c94d346974900993136a5c3e78cc07b537c750f
solarShivam <solarshivam9@gmail.com>
2025-07-24 13:57:42 +0530

    files of the assets

---
+++ assets/trending-up-Cnnkc7b-.js
@@ -0,0 +1,11 @@
+import{c}from"./index-DlEiKHF4.js";/**
+ * @license lucide-react v0.513.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"m15.477 12.89 1.515 8.526a.5.5 0 0 1-.81.47l-3.58-2.687a1 1 0 0 0-1.197 0l-3.586 2.686a.5.5 0 0 1-.81-.469l1.514-8.526",key:"1yiouv"}],["circle",{cx:"12",cy:"8",r:"6",key:"1vp47v"}]],t=c("award",o);/**
+ * @license lucide-react v0.513.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=[["path",{d:"M16 7h6v6",key:"box55l"}],["path",{d:"m22 7-8.5 8.5-5-5L2 17",key:"1t1m79"}]],n=c("trending-up",a);export{t as A,n as T};
