a97e5ba91a65acb939bae8e38f0b4af58fdacdab
solarShivam <solarshivam9@gmail.com>
2025-07-24 14:25:44 +0530

    runnable dist setup

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  base: './', // makes build portable on any repo and local
 })
